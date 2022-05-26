f8b1239252034e4cccb56516fcb7062be6b8312d
Pradhumna Pancholi <pnpancholi@protonmail.com>
2022-05-25 23:13:09 -0400

    test setup, added withdraw function, and some refactors

--- src/Space.sol
+++ src/Space.sol
@@ -1,21 +1,23 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.13;
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity 0.8.10;
 
 import "openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "openzeppelin-contracts/contracts/access/Ownable.sol";
 import "openzeppelin-contracts/contracts/utils/Counters.sol";
 
-contract Bozo is ERC721URIStorage, Ownable{
+contract Space is ERC721URIStorage, Ownable{
 		using Counters for Counters.Counter;
 		Counters.Counter private _tokenId;
 
 		bool public paused = false; //default value is false //
 
-		uint256 public COST = 0.69 ether; //dummy value for now//
-		uint256 constant MAX_SUPPLY = 10000; //dummy value for now//
+		uint256 public constant COST = 0.69 ether; //dummy value for now//
+		uint256 public constant MAX_SUPPLY = 10000; //dummy value for now//
 
-		constructor() ERC721("Bozo", "BOZO") {}
+		constructor() ERC721("Space", "SPACE") {}
 
+		event Withdraw(address _to, uint256 _amount);
+		
 		function pause() external returns (bool) {
 				paused = true;
 				return paused;
@@ -26,8 +28,12 @@ contract Bozo is ERC721URIStorage, Ownable{
 				return paused;
 		}
 
-		function withdraw() external returns (bool) {
-				return false;
+		function withdraw() external onlyOwner {
+				uint256 balance = address(this).balance;
+				require(balance > 0, "No more funds left to withdraw");
+				(bool success, ) = (msg.sender).call{value: balance}("");
+				require(success, "Withdrawal Failed!");
+				emit Withdraw(msg.sender, balance);
 		}
 
 		function mint() public returns(uint256){
---
+++ test/Space.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier : AGPL-3.0
+pragma solidity 0.8.10;
+
+import "forge-std/Test.sol";
+import "../src/Space.sol";
+
+contract SpaceTest is Test {
+		function setup() public {}
+}
